3d8d2cf25353535e9f8d34d85c9071e7e38845d2
Robert Parker <rob@parob.com>
2026-09-16 10:33:58 +0100

    Remove misleading camera power switches from collapsed tiles

--- src/components/widgets/DoorbellWidget.tsx
+++ src/components/widgets/DoorbellWidget.tsx
@@ -7,10 +7,13 @@ import { useHomeCamerasEnabled } from '@/hooks/useHomeCamerasEnabled';
 import { useCameraTileExpansion } from '@/hooks/useCameraTileExpansion';
 import { CameraTileFrame } from './CameraTileFrame';
 import { CameraTilePreview } from './CameraTilePreview';
+import { CameraModeControls } from './CameraModeControls';
 import { isCommunity } from '@/lib/config';
 
 export const DoorbellWidget: React.FC<WidgetProps> = memo(({
   accessory,
+  onToggle,
+  getEffectiveValue,
   compact,
   expanded,
   onExpandToggle,
@@ -100,7 +103,9 @@ export const DoorbellWidget: React.FC<WidgetProps> = memo(({
       onToggleShowHidden={onToggleShowHidden}
       onShare={onShare}
       locationSubtitle={locationSubtitle}
-    />
+    >
+      {preview.expanded && <CameraModeControls accessory={accessory} onToggle={onToggle} getEffectiveValue={getEffectiveValue} />}
+    </WidgetCard>
     </CameraTileFrame>
   );
 });
